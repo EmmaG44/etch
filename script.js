8d21bc80445592e9e25ae866117ffeeb09ef072c
EmmaG44 <emma.gunasekera@gmail.com>
2023-12-01 12:39:08 +0000

    loop to add div to boxes div

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //set up 16x16 grid
-function setUpGrid() {
+/*function setUpGrid() {
 
     for (let i = 0; i < 16 * 16; i++) {
         const gridElement = document.createElement('div');
@@ -7,3 +7,19 @@ function setUpGrid() {
         grid.appendChild(gridElement);
     }
 }
+
+
+window.onload = () => {
+    setUpGrid();
+}*/
+
+const boxes = document.querySelector('#boxes');
+
+for (i = 0; i < 4; i++) {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    //square.setAttribute('draggable', 'false');
+    square.style.color = 'blue';
+    boxes.appendChild(square);
+    //console.log("h");
+};
